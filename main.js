2d3d68d2e2262acc75c1e1ab63210dc38725f2a4
maxfox97 <max.lisicin@gmail.com>
2021-10-13 14:39:49 +0300

    change

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]);
 
 function moveZeros(arr) {
-    let arrValues = [];
-    let arrZero = [];
-    arr.forEach(element => {
-        if (element !== 0) {
-            arrValues.push(element);
-        } else {
-            arrZero.push(element);
-        }
-    });
-    let merged = [...arrValues, ...arrZero];
-    alert(merged);
+    const arrValues = [];
+    const arrZero = [];
+    let merged = [];
+    arr.forEach(element => { element !== 0 ? arrValues.push(element) : arrZero.push(element); });
+    merged = [...arrValues, ...arrZero];
+    console.log(merged);
 };
